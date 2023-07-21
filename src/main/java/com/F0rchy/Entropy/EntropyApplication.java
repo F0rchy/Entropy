c530ba901f995cd85f31631898e578885163d78f
F0rchy <theartemka6@mail.ru>
2023-07-21 14:58:26 +0300

    Start with spring initializr

---
+++ src/main/java/com/F0rchy/Entropy/EntropyApplication.java
@@ -0,0 +1,13 @@
+package com.F0rchy.Entropy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntropyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntropyApplication.class, args);
+	}
+
+}
