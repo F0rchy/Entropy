51c9071abca6dc0b7a3193aeaa3039f018d214ad
F0rchy <theartemka6@mail.ru>
2023-07-23 17:39:45 +0300

    Sites and thymeleaf blocks

--- src/main/java/com/F0rchy/Entropy/controllers/MainController.java
+++ src/main/java/com/F0rchy/Entropy/controllers/MainController.java
@@ -12,4 +12,16 @@ public class MainController {
         model.addAttribute("title", "Главная страница");
         return "home";
     }
+
+    @GetMapping("/articles")
+    public String articles(Model model) {
+        model.addAttribute("title", "Игровые статьи");
+        return "articles";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "О нас");
+        return "about";
+    }
 }
---
+++ src/main/java/com/F0rchy/Entropy/controllers/NewsController.java
@@ -0,0 +1,15 @@
+package com.F0rchy.Entropy.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NewsController {
+
+    @GetMapping("/news")
+    public String news(Model model) {
+        model.addAttribute("title", "Новости сайта");
+        return "news";
+    }
+}
